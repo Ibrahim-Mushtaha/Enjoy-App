b9bfce63d1ec8a164e0b1d323cca00c853972854
ibrahim mushtaha <ibrahim.mushtaha2@gmail.com>
2021-09-21 22:25:43 +0300

    update string constant

--- app/src/main/java/com/ix/ibrahim7/rxjavaapplication/other/StringConstant.kt
+++ app/src/main/java/com/ix/ibrahim7/rxjavaapplication/other/StringConstant.kt
@@ -3,8 +3,6 @@ package com.ix.ibrahim7.rxjavaapplication.other
 const val ZERO = 0
 const val ONE = 1
 const val TWO = 2
-const val THREE = 3
-const val FOUR = 4
 
 const val BASE_URL = "https://api.themoviedb.org/3/"
 const val ACCESSTOKEN = "AccessTOKEN"
@@ -13,9 +11,6 @@ const val CONTENT_TYPE = "text/plain"
 const val PICKLANG = "picklang"
 const val ALERT = "alert"
 const val LANG = "lang"
-const val NOTIFICATION = "notification"
-const val TURN_ON = "turn_on"
-const val TURN_OFF = "turn_off"
 const val EN = "en"
 const val AR = "ar"
 
